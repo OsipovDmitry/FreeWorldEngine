e6e7fba93b6a19cba8bc7a31421553757b152b6a
vshturmin <vlshturmin@gmail.com>
2016-09-30 20:40:29 +0500

    update Types

--- include/Types.h
+++ include/Types.h
@@ -3,6 +3,9 @@
 
 #include <map> 
 #include <string>
+#include <initializer_list>
+
+#include "CoreSettings.h"
 
 typedef signed char            int8;
 typedef signed short int       int16;
@@ -51,99 +54,67 @@ struct DateTime {
 	Time time;
 };
 
-/*struct VertexFormat { // Все значения указываются в штуках float. Если size == 0, то соответсвующего атрибута нет.
-	uint8 positionSize, positionStride;
-	uint8 tangentSize, tangentStride;
-	uint8 binormalSize, binormalStride;
-	uint8 normalSize, normalStride;
-	uint8 texCoord0Size, texCoord0Stride;
-	uint8 texCoord1Size, texCoord1Stride;
-	uint8 texCoord2Size, texCoord2Stride;
-	uint8 texCoord3Size, texCoord3Stride;
-
-	VertexFormat() : positionSize(0), positionStride(0),
-					 tangentSize(0), tangentStride(0),
-					 binormalSize(0), binormalStride(0),
-					 normalSize(0), normalStride(0), 
-					 texCoord0Size(0), texCoord0Stride(0), 
-					 texCoord1Size(0), texCoord1Stride(0),
-					 texCoord2Size(0), texCoord2Stride(0),
-					 texCoord3Size(0), texCoord3Stride(0) {}
-
-}; // struct VertexFormat
-
-struct TriMesh {
-	float *pVertexData;
-	uint32 *pIndexData;
-	uint32 numVertices;
-	uint32 numIndices;
-	VertexFormat vertexFormat;
-
-	TriMesh() : pVertexData(0), pIndexData(0), numVertices(0), numIndices(0), vertexFormat() {} 
-
-}; // struct TriMesh
-*/
-
-/*class VertexFormat {
+struct VertexFormatPrivate;
+class /*CORE_FUNC_DLL*/ VertexFormat {
 public:
-	enum AttributeType {
-		AttributeType_Position,
-		AttributeType_Tangent,
-		AttributeType_Binormal,
-		AttributeType_Normal,
-		AttributeType_Tangent,
-		AttributeType_TexCoord0,
-		AttributeType_TexCoord1,
-		AttributeType_TexCoord2,
-		AttributeType_TexCoord3,
-	};
-	struct Attribute {
-		uint8 size, offset;
-		Attribute(const uint8 theSize, const uint8 theOffset) : size(theSize), offset(theOffset) {}
+    enum class AttributeType {
+        Position,
+        Tangent,
+        Binormal,
+        Normal,
+        TexCoord0,
+        TexCoord1,
+        TexCoord2,
+        TexCoord3,
 	};
 
-	VertexFormat() : m_attributes() {}
-	void addAttribute(const AttributeType type, const Attribute attrib) { m_attributes[type] = attrib; }
-	Attribute attribute(const AttributeType type) const;
+    class Attribute {
+    public:
+        uint8 size{0};
+        uint8 offset{0};
+
+        Attribute() = default;
+        Attribute(const Attribute&) = default;
+        Attribute(uint8 size, uint8 offset);
+        Attribute(std::initializer_list<uint8>);
+
+        bool isNull() const;
+	};
+
+    VertexFormat();
+    VertexFormat(const VertexFormat&);
+    VertexFormat(std::initializer_list<std::pair<AttributeType, Attribute>>);
+
+    void addAttribute(const std::pair<AttributeType, Attribute>&);
+    void addAttribute(AttributeType type, const Attribute& attrib);
+    Attribute attribute(AttributeType type) const;
 	void clear();
 
-	static VertexFormat V3() { 
-		VertexFormat vf;
-		vf.addAttribute(AttributeType_Position, Attribute(3, 0));
-		return vf;
-	}
-
-	static VertexFormat T2V3() { 
-		VertexFormat vf;
-		vf.addAttribute(AttributeType_TexCoord0, Attribute(2, 0));
-		vf.addAttribute(AttributeType_Position, Attribute(3, 2));
-		return vf;
-	}
+    static VertexFormat V3();
+    static VertexFormat T2V3();
 
 private:
-	std::map<AttributeType, Attribute> m_attributes;
-
+    VertexFormatPrivate *m;
 };
 
 struct Mesh {
-	enum PrimitiveFormat {
-		PrimitiveFormat_None,
-		PrimitiveFormat_Points,
-		PrimitiveFormat_Lines,
-		PrimitiveFormat_LineStrip,
-		PrimitiveFormat_LineLoop,
-		PrimitiveFormat_Trangles,
-		PrimitiveFormat_TriangleStrip,
-		PrimitiveFormat_TrangleFan
+    enum class PrimitiveFormat {
+        None,
+        Points,
+        Lines,
+        LineStrip,
+        LineLoop,
+        Trangles,
+        TriangleStrip,
+        TrangleFan
 	};
 
-	float *pVertexData;
-	uint32 *pIndicesData;
-	uint32 numVertices, numIndices;
+    float *pVertexData{nullptr};
+    uint32 *pIndicesData{nullptr};
+    uint32 numVertices{0};
+    uint32 numIndices{0};
 	VertexFormat vertexFormat;
-	PrimitiveFormat primitiveFormat;
-
-	Mesh() : pVertices(0), pIndicesData(0), numVertices(0), numIndices(0), vertexFormat(0), primitiveFormat(PrimitiveFormat_None) {}
-};*/
+    PrimitiveFormat primitiveFormat{PrimitiveFormat::None};
+};
 
 #endif // __TYPES__
