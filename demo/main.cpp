1413e8f478709ca0d276a74f711d1e5021983461
OsipovDmitry <dmitry1992osipov@gmail.com>
2016-12-14 23:04:44 +0300

    update demo

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,41 +1,92 @@
 #include <FreeWorldEngine.h>
 using namespace FreeWorldEngine;
+#include <glm/gtc/matrix_transform.hpp>
 
-float vertices[] = {0.0f, 0.0f, -1.0f,
-					1.0f, 0.0f, -1.0f,
-					0.0f, 1.0f, -1.0f,
-					1.0f, 1.0f, -1.0f,};
+float vertices[] = {-1.0f, -1.0f, 0.0f,
+					1.0f, -1.0f, 0.0f,
+					-1.0f, 1.0f, 0.0f,
+					1.0f, 1.0f, 0.0f,};
 
 unsigned int indices[] = {0,1,2,3};
 
 std::string vShader =
 "#version 330 core\n"\
 "layout(location = 0) in vec3 pos;\n"\
+"uniform mat4 viewProjMatrix;\n"\
 "out vec2 tc;\n"\
 "void main(void)\n"\
 "{\n"\
-"	tc = pos.xy;\n"\
-"	gl_Position = vec4 (pos, 1.0);\n"\
+"	tc = pos.xy*0.5+vec2(0.5);\n"\
+"	gl_Position = viewProjMatrix * vec4 (pos, 1.0);\n"\
 "}\n";
 
 std::string fShader =
 "#version 330 core\n"\
 "uniform sampler2D tex;\n"\
 "in vec2 tc;\n"\
-"out vec4 outColor;\n"\
+"layout(location = 0) out vec4 outColor;\n"\
 "void main(void)\n"\
 "{\n"\
 "	outColor = texture(tex, tc);\n"\
+"}\n";
+
+std::string vShaderPost =
+"#version 330 core\n"\
+"layout(location = 0) in vec3 pos;\n"\
+"void main(void)\n"\
+"{\n"\
+"	gl_Position = vec4 (pos, 1.0);\n"\
+"}\n";
+
+std::string fShaderPost =
+"#version 330 core\n"\
+"uniform samplerRect tex;\n"\
+"uniform samplerRect texDepth;\n"\
+"out vec4 outColor;\n"\
+"void main(void)\n"\
+"{\n"\
+"	float zMin, zMax;\n"\
+"	zMin = zMax = texture(texDepth, gl_FragCoord.xy).r;\n"\
+"	for (int x = -2; x <=2; ++x)\n"\
+"		for (int y = -2; y <= 2; ++y) {\n"\
+"			float zVal = texture(texDepth, gl_FragCoord.xy+vec2(x,y)).r;\n"\
+"			if (zVal < zMin) zMin = zVal;\n"\
+"			else if (zVal > zMax) zMax = zVal;\n"\
+"		}\n"\
+"	outColor = texture(tex, gl_FragCoord.xy);\n"\
+"	if (abs(zMax-zMin) > 0.12) outColor = vec4(0,0,0,1);\n"\
 "}\n";
 
 IGPUProgram *pProgram;
 IGPUBufferContainer *pBCont;
 IGPUTexture *pTexture;
 
+IGPUFrameBuffer *pFrameBuffer;
+IGPUProgram *pProgramPost;
+IGPUBufferContainer *pBContPost;
+IGPUTexture *pTexturePost;
+IGPUTexture *pDepthTexPost;
+
 void render() {
-	getCoreEngine()->renderer()->tmp();
+	getCoreEngine()->renderer()->enableDepthTest();
+	getCoreEngine()->renderer()->setFrameBuffer(pFrameBuffer, 1);
+	pFrameBuffer->clearDepthBuffer();
+	pFrameBuffer->clearColorBuffer(0, 1,0,0,1);
+
+	glm::mat4 vpMatrix;
+	static float angle = 0.0f;
+	vpMatrix = glm::perspective(45.0f, 1.0f, 3.0f, 7.0f) * glm::translate(glm::mat4(), glm::vec3(0.0f, 0.0f, -5.0f)) * glm::rotate(glm::mat4(), angle, glm::vec3(0.0f, 1.0f, 0.0f));
+	angle += 0.01f;
+	pProgram->setUniform(pProgram->uniformLocationByName("viewProjMatrix"), vpMatrix);
+
 	getCoreEngine()->renderer()->setTexture(0, pTexture);
 	getCoreEngine()->renderer()->renderIndexedGeometry(pProgram, pBCont, PrimitiveFormat_TriangleStrip, TYPE_UNSIGNED_INT_32, 4, 0);
+	
+	getCoreEngine()->renderer()->disableDepthTest();
+	getCoreEngine()->renderer()->setFrameBuffer(0);
+	getCoreEngine()->renderer()->setTexture(0, pTexturePost);
+	getCoreEngine()->renderer()->setTexture(1, pDepthTexPost);
+	getCoreEngine()->renderer()->renderIndexedGeometry(pProgramPost, pBContPost, PrimitiveFormat_TriangleStrip, TYPE_UNSIGNED_INT_32, 4, 0);
 }
 
 int main() {
@@ -43,7 +94,7 @@ int main() {
 	ICore *p = getCoreEngine();
 	p->initialize();
 
-	p->logger()->printMessage("Hello!", ILogger::MessageType_Debug);
+	p->logger()->printMessage("Debug, Hello!", ILogger::MessageType_Debug);
 
 	IWindow *pMainWindow = p->mainWindow();
 	if (pMainWindow)
@@ -56,13 +107,11 @@ int main() {
 	if (!pVSh->compile(&sLog)) {
 		p->logger()->printMessage(sLog, ILogger::MessageType_Error);
 	}
-
 	IGPUShader *pFSh = p->renderer()->createShader(IGPUShader::IGPUShaderType_Fragment);
 	pFSh->loadFromData(fShader);
 	if (!pFSh->compile(&sLog)) {
 		p->logger()->printMessage(sLog, ILogger::MessageType_Error);
 	}
-
 	pProgram = p->renderer()->createProgram();
 	pProgram->attachShader(pVSh);
 	pProgram->attachShader(pFSh);
@@ -71,6 +120,24 @@ int main() {
 	}
 	pProgram->setUniform(pProgram->uniformLocationByName("tex"), 0);
 
+	IGPUShader *pVShPost = p->renderer()->createShader(IGPUShader::IGPUShaderType_Vertex);
+	pVShPost->loadFromData(vShaderPost);
+	if (!pVShPost->compile(&sLog)) {
+		p->logger()->printMessage(sLog, ILogger::MessageType_Error);
+	}
+	IGPUShader *pFShPost = p->renderer()->createShader(IGPUShader::IGPUShaderType_Fragment);
+	pFShPost->loadFromData(fShaderPost);
+	if (!pFShPost->compile(&sLog)) {
+		p->logger()->printMessage(sLog, ILogger::MessageType_Error);
+	}
+	pProgramPost = p->renderer()->createProgram();
+	pProgramPost->attachShader(pVShPost);
+	pProgramPost->attachShader(pFShPost);
+	if (!pProgramPost->link(&sLog)) {
+		p->logger()->printMessage(sLog, ILogger::MessageType_Error);
+	}
+	pProgramPost->setUniform(pProgramPost->uniformLocationByName("tex"), 0);
+	pProgramPost->setUniform(pProgramPost->uniformLocationByName("texDepth"), 1);
 
 	IImage *pImg = p->imageLoader()->loadImage("img.jpg");
 	
@@ -79,13 +146,29 @@ int main() {
 	pTexture = p->renderer()->createTexture(IGPUTexture::IGPUTextureType_2D, pImg->raster()->size, TextureFormat(TextureFormat::PixelFormat_NormalizeUnsigned, TextureFormat::ChannelSize_8, TextureFormat::ChannelsCount_3));
 	pTexture->setSubData(offs, pImg->raster()->size, TextureFormat::ChannelsCount_3, pImg->raster()->type, pImg->raster()->pData);
 
+	uint32 texsize[2] = {500,500};
+	pTexturePost = p->renderer()->createTexture(IGPUTexture::IGPUTextureType_Rectangle, texsize, TextureFormat(TextureFormat::PixelFormat_NormalizeUnsigned, TextureFormat::ChannelSize_8, TextureFormat::ChannelsCount_3));
+	pDepthTexPost = p->renderer()->createTexture(IGPUTexture::IGPUTextureType_Rectangle, texsize, TextureFormat(TextureFormat::PixelFormat_SpecialDepth, TextureFormat::ChannelSize_32, TextureFormat::ChannelsCount_1));
+
 	IGPUBuffer *pVB = p->renderer()->createBuffer(12 * sizeof(float), IGPUBuffer::IGPUBufferUsage_StaticDraw, vertices);
 	IGPUBuffer *pFB = p->renderer()->createBuffer(4 * sizeof(int), IGPUBuffer::IGPUBufferUsage_StaticDraw, indices);
-
 	pBCont = p->renderer()->createBufferContainer();
 	pBCont->setVertexAttribute(pVB, 0, 3, 0, 0, TYPE_FLOAT);
 	pBCont->enableVertexAttribute(0);
 	pBCont->setIndexBuffer(pFB);
+
+	IGPUBuffer *pVBPost = p->renderer()->createBuffer(12 * sizeof(float), IGPUBuffer::IGPUBufferUsage_StaticDraw, vertices);
+	IGPUBuffer *pFBPost = p->renderer()->createBuffer(4 * sizeof(int), IGPUBuffer::IGPUBufferUsage_StaticDraw, indices);
+	pBContPost = p->renderer()->createBufferContainer();
+	pBContPost->setVertexAttribute(pVBPost, 0, 3, 0, 0, TYPE_FLOAT);
+	pBContPost->enableVertexAttribute(0);
+	pBContPost->setIndexBuffer(pFBPost);
+
+	pFrameBuffer = p->renderer()->createFrameBuffer();
+	pFrameBuffer->attachColorBuffer(0, pTexturePost);
+	pFrameBuffer->attachDepthBuffer(pDepthTexPost);
+
+	getCoreEngine()->renderer()->tmp();
 
 	p->windowManager()->mainLoop();
 	p->deinitialize();
